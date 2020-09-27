85af2856a6f76c53ffb1e5cf2bd6cd5806398118
seremwen <seremwe.81@gmail.com>
2020-09-27 16:29:25 +0200

    new message

--- src/main/java/com/mycompany/mavenproject1/Main.java
+++ src/main/java/com/mycompany/mavenproject1/Main.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject1;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing....");
+        System.out.println("No more testing!");
     }
 }
